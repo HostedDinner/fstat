49c02d3da46c9c0b0d3014af223513e3b7b6b0e8
hosteddinner@gmail.com <HostedDinner@users.noreply.github.com>
2011-04-19 14:54:37 +0000

    * Credits and Version (First "Version Bump" ;) )

--- elements/layout_footer.php
+++ elements/layout_footer.php
@@ -1,3 +1,4 @@
 	<div class="footer">
+		<a href="http://code.google.com/p/fstat/">FStat</a> Version <?php echo $fstat_fstat_version; ?> from F-Soft by <a href="http://fa-ne.tk/">Fabian Neffgen</a><br>
 		<?php echo FLANG_H_USEDBY; ?> <a href="http://user-agent-string.info/">UAString Api</a>
 	</div>
