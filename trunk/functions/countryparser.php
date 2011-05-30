ff9bb54256d48e9b7e246100f67b600418dbdf98
hosteddinner@gmail.com <hosteddinner@gmail.com@d55d6cad-90fa-a05e-55e6-0ab1a55a7b0b>
2011-05-30 18:10:44 +0000

    *country parser will check if ip database is present (Full/Lite Install) to supress warnings

--- trunk/functions/countryparser.php
+++ trunk/functions/countryparser.php
@@ -23,42 +23,44 @@ function CountryParse(){
 	
 	
 function CountryParseHelper($filename, $lookupip_long){
-	$handle = fopen($filename,"r");
-		
-	if($handle){
-		//Seek to the end
-		fseek($handle, 0, SEEK_END);
-		$high = ftell($handle);
-		$low = 0;
-		
-		while ($low <= $high) {
-			$mid = floor(($low + $high) / 2);
-	
-			fseek($handle, $mid);
-
-			if($mid != 0){
-				//Read a line to move to eol
-				$line = fgets($handle);
-			}
-       
-			//Read a line to get data
-			$data = fgetcsv ($handle, 200, ",");
+	if(is_readable($filename)){
+		$handle = fopen($filename,"r");
 			
-			if(($data[0] <= $lookupip_long) and ($lookupip_long <= $data[1])){
-				$returnar['icon'] = strtolower($data[2]).".png";
-				$returnar['name'] = ucwords(strtolower($data[4]));
-				fclose($handle);
-				return $returnar;
-			}else{
-				if ($lookupip_long < $data[0]){
-					$high = $mid - 1;
+		if($handle){
+			//Seek to the end
+			fseek($handle, 0, SEEK_END);
+			$high = ftell($handle);
+			$low = 0;
+			
+			while ($low <= $high) {
+				$mid = floor(($low + $high) / 2);
+		
+				fseek($handle, $mid);
+	
+				if($mid != 0){
+					//Read a line to move to eol
+					$line = fgets($handle);
+				}
+		
+				//Read a line to get data
+				$data = fgetcsv ($handle, 200, ",");
+				
+				if(($data[0] <= $lookupip_long) and ($lookupip_long <= $data[1])){
+					$returnar['icon'] = strtolower($data[2]).".png";
+					$returnar['name'] = ucwords(strtolower($data[4]));
+					fclose($handle);
+					return $returnar;
 				}else{
-					$low = $mid + 1;
+					if ($lookupip_long < $data[0]){
+						$high = $mid - 1;
+					}else{
+						$low = $mid + 1;
+					}
 				}
 			}
 		}
+		fclose($handle);
 	}
-	fclose($handle);
 	return false;
 }
 	
