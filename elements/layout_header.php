e68ab6fbd737562e04c93e73ebc31e269bf9d202
hosteddinner@gmail.com <HostedDinner@users.noreply.github.com>
2011-06-19 12:43:48 +0000

    * add stylsheet for mobile devices (mainly killing the floatstyle and and fixing the menu)

--- elements/layout_header.php
+++ elements/layout_header.php
@@ -1,7 +1,9 @@
 	<title><?php echo $fstat_title; ?></title>
 	<meta name="author" content="Fabian Neffgen">
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-	<link rel="stylesheet" type="text/css" href="./style.css">
+	<link rel="stylesheet" media="all" type="text/css" href="./style.css">
+	<link rel="stylesheet" media="screen and (min-device-width: 900px)" type="text/css" href="./style.desktop.css">
+	<link rel="stylesheet" media="handheld, screen and (max-device-width: 899px)" type="text/css" href="./style.mobile.css">
 	<link rel="alternate" type="application/atom+xml" href="./feed.php" title="FStat Monthly Update">
 	<script type="text/javascript">
 		//I will not support a IE fix!!!!
