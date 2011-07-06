f0e7e9683887b3c68fc36d6bf318fef9efbdd463
hosteddinner@gmail.com <hosteddinner@gmail.com@d55d6cad-90fa-a05e-55e6-0ab1a55a7b0b>
2011-07-06 15:57:54 +0000

    * show in last visiors the sites with Slashs (/) correctly * escape html entities and | char in path-files (only new ones) * small correction of including mobile css style * update uasdata.ini

--- trunk/elements/layout_header.php
+++ trunk/elements/layout_header.php
@@ -3,7 +3,7 @@
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 	<link rel="stylesheet" media="all" type="text/css" href="./style.css">
 	<link rel="stylesheet" media="screen and (min-device-width: 900px)" type="text/css" href="./style.desktop.css">
-	<link rel="stylesheet" media="handheld, screen and (max-device-width: 899px)" type="text/css" href="./style.mobile.css">
+	<link rel="stylesheet" media="handheld, only screen and (max-device-width: 899px)" type="text/css" href="./style.mobile.css">
 	<link rel="alternate" type="application/atom+xml" href="./feed.php" title="FStat Monthly Update">
 	<script type="text/javascript">
 		//I will not support a IE fix!!!!
