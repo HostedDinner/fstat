18915528aa06475d0cd50a4cbcd8db93334d482e
hosteddinner@gmail.com <hosteddinner@gmail.com@d55d6cad-90fa-a05e-55e6-0ab1a55a7b0b>
2011-09-05 19:20:06 +0000

    * most of backend is handled by all.php (bots.php, br_os.php, browser.php, counter.php, country.php, os.php, referer.php, search.php and time.php fetch conten from there) * updated help.txt

--- trunk/backend/counter.php
+++ trunk/backend/counter.php
@@ -9,100 +9,11 @@ if(!isset($is_include)){
 	$prefolder = "./";
 }
 
-include $prefolder."config/settings.php";
-include $prefolder."functions/backend_include.php";
+$fstat_backend_get_list = "counter";
 
-$count_bots = 0;
-$count_people = 0;
-
-//Ausgabe vorbereiten:
-$xmlausgabe = new DOMDocument('1.0', 'UTF-8');
-	$xmlausgabe->xmlStandalone = true;
-	$xmlausgabe->preserveWhiteSpace = false;
-	$xmlausgabe->formatOutput = true;
-$root = $xmlausgabe->createElement("list");
-$root = $xmlausgabe->appendChild($root);
-
-//extra kind of root for Counter
-$cot_root = $xmlausgabe->createElement("counter");
-$cot_root = $root->appendChild($cot_root);
-
-for($y = gmdate("Y", $fstat_backend_start_timestamp); $y <= gmdate("Y", $fstat_backend_end_timestamp); $y++){
-	if($y == gmdate("Y", $fstat_backend_start_timestamp)){
-		$m = gmdate("n", $fstat_backend_start_timestamp);
-	}else{
-		$m = 1;
-	}
-	while($m <= 12){
-		for($i = 1; $i <= 31; $i++){//exact days of month would be only slightly faster
-			$filename = $prefolder.$fstat_data_dir."stat/".$y."/".str_pad($m,2,"0",STR_PAD_LEFT)."/".str_pad($i,2,"0",STR_PAD_LEFT).".xml";
-		
-			$tmp_count_bots = 0;
-			$tmp_count_bots2 = 0;
-			$tmp_count_people = 0;
-			
-			if(is_file($filename)){
-				$xmldoc = new DOMDocument();
-				$xmldoc->load($filename);
-				
-				$xpath = new  DOMXPath($xmldoc);
-				
-				//Robots:
-				$query = 'count(//typ[.="Robot"])';
-				$tmp_count_bots = $xpath->evaluate($query, $xmldoc);
-				$count_bots = $count_bots + $tmp_count_bots;
-				$query = 'count(//typ[.="Validator"])';
-				$tmp_count_bots2 = $xpath->evaluate($query, $xmldoc);
-				$count_bots = $count_bots + $tmp_count_bots2;
-				
-				//People
-				$query = 'count(//visitor)';
-				$tmp_count = $xpath->evaluate($query, $xmldoc);
-				$tmp_count_people = $tmp_count - $tmp_count_bots - $tmp_count_bots2;
-				$count_people = $count_people + $tmp_count_people;
-				
-			}
-			
-			$dayadd = $xmlausgabe->createElement("day");
-				$dayadd->setAttribute("id", $i);
-				$dayadd->appendChild($xmlausgabe->createElement('bots', ($tmp_count_bots + $tmp_count_bots2)));
-				$dayadd->appendChild($xmlausgabe->createElement('people', $tmp_count_people));
-			
-			$cot_root->appendChild($dayadd);
-		}
-		$m++;
-		if($y == gmdate("Y", $fstat_backend_end_timestamp) and $m > gmdate("n", $fstat_backend_end_timestamp)){
-			$m = 13;
-		}
-	}
-}
-
-
-$gesamtadd = $xmlausgabe->createElement("total");
-	$gesamtadd->appendChild($xmlausgabe->createElement('bots', $count_bots));
-	$gesamtadd->appendChild($xmlausgabe->createElement('people', $count_people));
-
-$cot_root->appendChild($gesamtadd);
-
-echo $xmlausgabe->saveXML();
-
-//Aufbau der XML Datei
-//<list>
-//  <counter>
-//    <day id="1">
-//      <bots>3</bots>
-//      <people>3</people>
-//    </day>
-//    <day id="2">
-//      <bots>4</bots>
-//      <people>1</people>
-//    </day>
-//    ...
-//    <total>
-//      <bots>15</bots>
-//      <people>27</people>
-//    </total>
-//  </counter>
-//</list>
-
+ob_start();
+	include $prefolder."backend/all.php";
+	$contents = ob_get_contents();
+ob_end_clean();
+echo $contents;
 ?>
--- trunk/backend/search.php
+++ trunk/backend/search.php
@@ -9,87 +9,11 @@ if(!isset($is_include)){
 	$prefolder = "./";
 }
 
-include $prefolder."config/settings.php";
-include $prefolder."functions/backend_include.php";
+$fstat_backend_get_list = "search";
 
-$key_arr = array();
-
-for($y = gmdate("Y", $fstat_backend_start_timestamp); $y <= gmdate("Y", $fstat_backend_end_timestamp); $y++){
-	if($y == gmdate("Y", $fstat_backend_start_timestamp)){
-		$m = gmdate("n", $fstat_backend_start_timestamp);
-	}else{
-		$m = 1;
-	}
-	while($m <= 12){
-		for($i = 1; $i <= 31; $i++){//exact days of month would be only slightly faster
-			$filename = $prefolder.$fstat_data_dir."stat/".$y."/".str_pad($m,2,"0",STR_PAD_LEFT)."/".str_pad($i,2,"0",STR_PAD_LEFT).".xml";
-			
-			if(is_file($filename)){
-				$xmldoc = new DOMDocument();
-				$xmldoc->load($filename);
-				
-				$nodelist = $xmldoc->getElementsByTagName("visitor");
-				
-				foreach($nodelist as $visitor){
-					$typ = @$visitor->getElementsByTagName("typ")->item(0)->nodeValue;
-					if(($fstat_show_bots_as_visitors) or ($typ != "Robot" and $typ != "Validator")){
-						$key = @$visitor->getElementsByTagName("rkey")->item(0)->nodeValue;
-		
-						if((isset($key)) and ($key != "")){
-							if(!isset($key_arr[$key])){
-								$key_arr[$key] = 1;
-							}else{
-								$key_arr[$key] = $key_arr[$key] + 1;
-							}
-						}
-					}
-				}
-				
-			}
-		}
-		$m++;
-		if($y == gmdate("Y", $fstat_backend_end_timestamp) and $m > gmdate("n", $fstat_backend_end_timestamp)){
-			$m = 13;
-		}
-	}
-}
-
-arsort($key_arr);
-
-
-//Ausgabe vorbereiten:
-$xmlausgabe = new DOMDocument('1.0', 'UTF-8');
-	$xmlausgabe->xmlStandalone = true;
-	$xmlausgabe->preserveWhiteSpace = false;
-	$xmlausgabe->formatOutput = true;
-$root = $xmlausgabe->createElement("list");
-$root = $xmlausgabe->appendChild($root);
-
-//extra kind of root for Search
-$sea_root = $xmlausgabe->createElement("search");
-$sea_root = $root->appendChild($sea_root);
-
-foreach($key_arr as $ref => $count){
-	$refadd = $xmlausgabe->createElement("ref");
-	$refadd->setAttribute("keywords", $ref);
-		$refadd->appendChild($xmlausgabe->createElement("count", $count));
-	$sea_root->appendChild($refadd);
-}
-
-
-
-echo $xmlausgabe->saveXML();
-
-//Aufbau der XML Datei
-//<list>
-//  <search>
-//    <ref keywords="RWCP">
-//      <count>6</count>
-//    </ref>
-//    <ref keywords="Witzbox">
-//      <count>3</count>
-//    </ref>
-//</search>
-//</list>
-
+ob_start();
+	include $prefolder."backend/all.php";
+	$contents = ob_get_contents();
+ob_end_clean();
+echo $contents;
 ?>
