45c40562f801147369fbc7362cefd3687a75fd60
HostedDinner <HostedDinner@users.noreply.github.com>
2012-02-24 23:46:33 +0000

    * add Hungarian language (found at http://php-script.hu/script/bovebben/fstat) thanks for the translation

---
+++ lang/lang.hu.php
@@ -0,0 +1,81 @@
+<?php
+	//Monthnames
+	define('FLANG_JAN', "Január");
+	define('FLANG_FEB', "Február");
+	define('FLANG_MAR', "Március");
+	define('FLANG_APR', "Április");
+	define('FLANG_MAY', "Május");
+	define('FLANG_JUN', "Június");
+	define('FLANG_JUL', "Július");
+	define('FLANG_AUG', "Augusztus");
+	define('FLANG_SEP', "Szeptember");
+	define('FLANG_OKT', "Október");
+	define('FLANG_NOV', "November");
+	define('FLANG_DEC', "December");
+	
+	define('FLANG_DAY', "Nap");
+	define('FLANG_MONTH', "Hónap");
+	define('FLANG_YEAR', "Év");
+	
+	define('FLANG_TODAY', "Ma");
+	define('FLANG_PREV_MONTH', "Előző hónap");
+	define('FLANG_NEXT_MONTH', "Következő hónap");
+	
+	//Headers:
+	define('FLANG_H_ABOUT_FSTAT', "Az FStatról");
+	define('FLANG_H_LAST', "List of the last");
+	define('FLANG_H_STATFOR', "Statisztika:");
+	define('FLANG_H_OVERVIEW', "Áttekintés");
+	define('FLANG_H_DIST_VISIT', "Látogatók");
+	define('FLANG_H_DIST_TIME', "Időintervallum");
+	
+	define('FLANG_H_ABOUT', "Az FStatról");
+	define('FLANG_H_BOT', "Botok");
+	define('FLANG_H_BROWSER', "Böngészők");
+	define('FLANG_H_COUNTRY', "Országok");
+	define('FLANG_H_REFERER', "Forgalmi források");
+	define('FLANG_H_OS', "OS");
+	define('FLANG_H_SEARCHW', "Kulcsszavak");
+	define('FLANG_H_SITE', "Oldalak");
+	
+	
+	define('FLANG_BROWSER', "Böngésző");
+	define('FLANG_COUNTRY', "Ország");
+	define('FLANG_DATE', "Dátum");
+	define('FLANG_DOMAIN', "Domain");
+	define('FLANG_OS', "OS");
+	define('FLANG_SEARCHW', "Keresett kifejezés");
+	define('FLANG_SITE', "Oldal");
+	define('FLANG_TIME', "Idő");
+	
+	//Other
+	define('FLANG_LANG', "Nyelv");
+	
+	define('FLANG_COUNT', "Számláló");
+	define('FLANG_GRAPH', "Diagramm");
+	
+	define('FLANG_BOT_L', "Botok");
+	define('FLANG_BOT_S', "Botok");
+	define('FLANG_VISITOR_L', "Látogatók");
+	define('FLANG_VISITOR_S', "Lát.");
+	
+	define('FLANG_LAST', "Utolsó");
+	
+	define('FLANG_NODATA', "Nincsen elérhető adat!");
+	
+	//Tooltips
+	define('FLANG_VISITTIME_D', "Látogatók");
+	define('FLANG_VISITTIME_T', "Látogatók");
+	define('FLANG_VISITTIME_B', "Botok");
+	define('FLANG_CLOCK', "");
+	
+	//Footer/About
+	define('FLANG_H_USEDBY', "This statistic uses the");
+	define('FLANG_H_IPBY', "IP-to-Country Database by");
+	define('FLANG_H_FLAGBY', "and Flag Icons from");
+	define('FLANG_FEEDBACK', "Hiba jelentése vagy visszajelzés küldése ide:");
+	define('FLANG_LAST_UPDATE', "Last Update on");
+	
+	
+	
+?>
