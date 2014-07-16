255fcc67b41cd8dbdd69bd82ce306c7c33899a74
hosteddinner@gmail.com <hosteddinner@gmail.com@d55d6cad-90fa-a05e-55e6-0ab1a55a7b0b>
2014-07-16 09:31:17 +0000

    * reworked last / lastbot => merged in one file

--- trunk/elements/last.php
+++ trunk/elements/last.php
@@ -1,86 +1,74 @@
-	<div class="border">
-		<table class="Auflistung striped">
-			<tr>
-				<th>&nbsp;</th>
-				<th><?php echo FLANG_DATE; ?>:</th>
-				<th><?php echo FLANG_TIME; ?>:</th>
-				<th colspan="2"><?php echo FLANG_COUNTRY; ?>:</th>
-				<th colspan="2"><?php echo FLANG_OS; ?>:</th>
-				<th colspan="2"><?php echo FLANG_BROWSER; ?>:</th>
-				<th><?php echo FLANG_DOMAIN; ?>:</th>
-				<th><?php echo FLANG_H_SITE; ?>:</th>
-			</tr>
+    <div class="border">
+        <table class="Auflistung striped">
+            <tr>
+                <th>&nbsp;</th>
+                <th><?php echo FLANG_DATE; ?>:</th>
+                <th><?php echo FLANG_TIME; ?>:</th>
+                <th colspan="2"><?php echo FLANG_COUNTRY; ?>:</th>
+<?php if($urlBuilder->getPage() == 'last'): ?>
+                <th colspan="2"><?php echo FLANG_OS; ?>:</th>
+<?php endif; ?>
+                <th colspan="2"><?php echo FLANG_BROWSER; ?>:</th>
+                <th><?php echo FLANG_DOMAIN; ?>:</th>
+                <th><?php echo FLANG_H_SITE; ?>:</th>
+            </tr>
 <?php
-	$xml_last = new DOMDocument();
+    require_once __DIR__ . "/../classes/xpathHelper.php";
+
+    $xml_last = new DOMDocument();
+    if($urlBuilder->getPage() == 'last'){
         $xml_last->loadXML(Backend::getXML("./backend/last.php"));
-	
-	$nodelist = $xml_last->getElementsByTagName("visitor");
-	
-	$tmpcount = 0;
-	foreach($nodelist as $row){
-		$tmpcount++;
-		
-		$v_typ = @$row->getElementsByTagName("typ")->item(0)->nodeValue;
-		$v_uas = @$row->getElementsByTagName("uas")->item(0)->nodeValue;
-		$v_uip = @$row->getElementsByTagName("uip")->item(0)->nodeValue;
-		$v_uhost = @$row->getElementsByTagName("uhost")->item(0)->nodeValue; if($v_uhost == ""){$v_uhost = $v_uip;};
-		$v_uti = @$row->getElementsByTagName("uti")->item(0)->nodeValue;
-		$v_ufam = @$row->getElementsByTagName("ufam")->item(0)->nodeValue;
-		$v_unam = @$row->getElementsByTagName("unam")->item(0)->nodeValue;
-		$v_uico = @$row->getElementsByTagName("uico")->item(0)->nodeValue;
-		$v_ofam = @$row->getElementsByTagName("ofam")->item(0)->nodeValue;
-		$v_onam = @$row->getElementsByTagName("onam")->item(0)->nodeValue;
-		$v_oico = @$row->getElementsByTagName("oico")->item(0)->nodeValue;
-		$v_ucoi = @$row->getElementsByTagName("ucoi")->item(0)->nodeValue;
-		$v_ucon = @$row->getElementsByTagName("ucon")->item(0)->nodeValue;
-		$v_rkey = @$row->getElementsByTagName("rkey")->item(0)->nodeValue;
-		$v_rdom = @$row->getElementsByTagName("rdom")->item(0)->nodeValue;
-		$pathnode = $row->getElementsByTagName("path")->item(0);
-			if($pathnode){
-				$sitenodelist = $pathnode->getElementsByTagName("site");
-			}else{
-				$sitenodelist = NULL;
-			}
-		$c_filename = $fstat_data_dir."stat/".date("Y/m/d", $v_uti).".xml";
-		
-		echo "\t\t\t<tr>\n";
-		echo "\t\t\t\t<td title=\"".$v_uip." (".$v_uhost.")\">".$tmpcount.".</td>\n";
-		echo "\t\t\t\t<td><a href=\"".$c_filename."\">".date("d.m.y", $v_uti)."</a></td>\n";
-		echo "\t\t\t\t<td>".date("H:i", $v_uti)."</td>\n";
-		echo "\t\t\t\t<td class=\"icell\"><div class=\"country_icon\" style=\"background-position: 0px ".Country::getCountryOffset($v_ucoi)."px;\" title=\"".$v_ucon."\"></div></td>\n";
-		echo "\t\t\t\t<td title=\"".$v_uip." (".$v_uhost.")\">".$v_ucon."</td>\n";
-		echo "\t\t\t\t<td class=\"icell\"><img src=\"".$fstat_ico_dir."os/".$v_oico."\" alt=\"*\" title=\"".$v_ofam."\" width=\"16\" height=\"16\"></td>\n";
-		echo "\t\t\t\t<td title=\"".$v_uas."\">".$v_onam."</td>\n";
-		echo "\t\t\t\t<td class=\"icell\"><img src=\"".$fstat_ico_dir."agent/".$v_uico."\" alt=\"*\" title=\"".$v_ufam."\" width=\"16\" height=\"16\"></td>\n";
-		echo "\t\t\t\t<td title=\"".$v_uas."\">".$v_unam."</td>\n";
-		echo "\t\t\t\t<td title=\"".$v_rkey."\">".$v_rdom."</td>\n";
-		
-		$tmp_last_1 = "";
-		$tmp_last_2 = "";
-		
-		if($sitenodelist){
-			echo "\t\t\t\t<td title=\"";
-			foreach($sitenodelist as $v_sites){
-				$val = $v_sites->nodeValue;
-				list($val1, $val2) = explode("/", trim($val), 2);
-				
-				if($tmp_last_1 != $val1){
-					echo $val1.": ".$val2.", ";
-				}else{
-					if($val2 != $tmp_last_2){
-						echo $val2.", ";
-					}
-				}
-				$tmp_last_1 = $val1;
-				$tmp_last_2 = $val2;
-			}
-			echo "\">".FLANG_H_SITE."</td>\n";
-		}else{
-			echo "\t\t\t\t<td></td>\n";
-		}
-	
-		echo "\t\t\t</tr>\n";
-	}
+    }else if($urlBuilder->getPage() == 'lastbots'){
+        $xml_last->loadXML(Backend::getXML("./backend/lastbots.php"));
+    }
+    
+    $xpath = new DOMXPath($xml_last);
+
+    $nodelist = $xpath->query('//visitor');
+
+    $tmpcount = 0;
 ?>
-		</table>
-	</div>
+<?php foreach ($nodelist as $row) :
+        $tmpcount++;
+
+        $v_typ = $xpath->evaluate('string(./typ/text())', $row);
+        $v_uas = $xpath->evaluate('string(./uas/text())', $row);
+        $v_uip = $xpath->evaluate('string(./uip/text())', $row);
+        $v_uhost = $xpath->evaluate('string(./uhost/text())', $row);
+        if ($v_uhost == "") { $v_uhost = $v_uip;}
+        $v_uti = $xpath->evaluate('string(./uti/text())', $row);
+        $v_ufam = $xpath->evaluate('string(./ufam/text())', $row);
+        $v_unam = $xpath->evaluate('string(./unam/text())', $row);
+        $v_uico = $xpath->evaluate('string(./uico/text())', $row);
+        $v_ofam = $xpath->evaluate('string(./ofam/text())', $row);
+        $v_onam = $xpath->evaluate('string(./onam/text())', $row);
+        $v_oico = $xpath->evaluate('string(./oico/text())', $row);
+        $v_ucoi = $xpath->evaluate('string(./ucoi/text())', $row);
+        $v_ucon = $xpath->evaluate('string(./ucon/text())', $row);
+        $v_rkey = $xpath->evaluate('string(./rkey/text())', $row);
+        $v_rdom = $xpath->evaluate('string(./rdom/text())', $row);
+        $sitenodelist = $xpath->query('./path/site', $row);
+        $c_filename = $fstat_data_dir . "stat/" . date("Y/m/d", $v_uti) . ".xml";
+?>
+            <tr>
+                <td title="<?php echo $v_uip." (".$v_uhost.")"; ?>"><?php echo $tmpcount; ?></td>
+                <td><a href="<?php echo $c_filename; ?>"><?php echo date("d.m.y", $v_uti); ?></a></td>
+                <td><?php echo date("H:i", $v_uti); ?></td>
+                <td class="icell"><div class="country_icon" style="background-position: 0px <?php echo Country::getCountryOffset($v_ucoi); ?>px" title="<?php echo $v_ucon; ?>"></div></td>
+                <td title="<?php echo $v_uip." (".$v_uhost.")"; ?>"><?php echo $v_ucon; ?></td>
+<?php if($urlBuilder->getPage() == 'last'): ?>
+                <td class="icell"><img src="<?php echo $fstat_ico_dir . "os/" . $v_oico; ?>" alt="*" title="<?php echo $v_ofam; ?>" width="16" height="16"></td>
+                <td title="<?php echo $v_uas; ?>"><?php echo $v_onam; ?></td>
+<?php endif; ?>
+                <td class="icell"><img src="<?php echo $fstat_ico_dir . "agent/" . $v_uico; ?>" alt="*" title="<?php echo $v_ufam; ?>" width="16" height="16"></td>
+                <td title="<?php echo $v_uas; ?>"><?php echo $v_unam; ?></td>
+                <td title="<?php echo $v_rkey; ?>"><?php echo $v_rdom; ?></td>
+<?php if ($sitenodelist->length != 0) : ?>
+                <td title="<?php echo XPathHelper::listSiteNodeValues($sitenodelist); ?>"><?php echo FLANG_H_SITE; ?></td>
+<?php else : ?>
+                <td></td>
+<?php endif; ?>
+            </tr>
+<?php endforeach; ?>
+        </table>
+    </div>
